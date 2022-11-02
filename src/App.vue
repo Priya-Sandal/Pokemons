47911f57902b02629603e953191595e9de1c7252
Priya-Sandal <priya@innotechteam.in>
2022-11-02 22:25:46 +0530

    poke

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,82 @@
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <!-- <suspense> -->
+
+  <pokemon-cards :pokemons="pokemons" @chosen="fetchEvolutions" :selectedId="selectedId" />
+
+  <pokemon-cards :pokemons="evolution" />
+  <div class="cards">
+    <card v-for="pokemon in evolution" :key="pokemon.id" @click="fetchEvolutions(pokemon)">
+      <template v-slot:title>
+        {{ pokemon.name }} #{{ pokemon.id }}
+      </template>
+
+      <template v-slot:content>
+        <img :src="pokemon.sprites">
+      </template>
+
+      <template v-slot:description>
+        <div v-for="type in pokemon.types" :key="type">
+          {{ type }}
+        </div>
+      </template>
+    </card>
+  </div>
+
+
 </template>
 
+
+<script>
+import { ref, onMounted } from 'vue';
+  import Card from './components/Card.vue';
+import PokemonCards from './components/PokemonCards.vue';
+const api = 'https://pokeapi.co/api/v2/pokemon'
+const IDS = [1, 4, 7]
+export default {
+  components: {
+    PokemonCards,
+    Card
+  },
+  setup() {
+    const pokemons = ref(null)
+    const evolution = ref(null)
+    const selectedId = ref(0)
+    const fetchEvolutions = async (pokemon) => {
+      evolution.value = await fetchData(
+        [pokemon.id + 1, pokemon.id + 2])
+      selectedId.value = pokemon.id
+    }
+
+    onMounted(async () => {
+      fetchData(IDS)
+      pokemons.value = await fetchData(IDS)
+    })
+    const fetchData = async (ids) => {
+      const responses = await Promise.all(
+        ids.map(id => window.fetch(`${api}/${id}`))
+      )
+      const json = await Promise.all(
+        responses.map(data => data.json())
+      )
+      return json.map(dataum => ({
+        id: dataum.id,
+        name: dataum.name,
+        sprites: dataum.sprites.other['official-artwork'].front_default,
+        types: dataum.types.map(type => type.type.name)
+      }))
+    }
+    return {
+      pokemons,
+      evolution,
+      selectedId,
+      fetchEvolutions,
+      fetchData
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
